d2130b076bbb4df5266ad88defa0f2f2939c7cd2
data-dev-arav <131908005+data-dev-arav@users.noreply.github.com>
2023-04-27 00:55:49 +0530

    Python Input Output

---
+++ python_input_output.py
@@ -0,0 +1,26 @@
+# Declare & Assign Variables in Python
+
+int_var = 10
+float_var = 10.25
+string_var = "Arvind" # or another way 'ineuron batch 2'
+bool_var = True
+
+
+# Function in Python for Output
+print("Hello World !!!")
+
+
+# Print variable values in Python
+print("Value of int_var=",int_var)
+print("Value of float_var=",float_var)
+print("Value of string_var=",string_var)
+print("Value of boolean_var=",bool_var)
+
+
+# How to take inputs in Python ?
+# We can use input() function.
+
+name = input()
+age = input()
+print("User age=",age)
+print("User name=",name)
