dd0fef8cba0aec4ab17f260c8b494b7397c969a3
Arvind Pradhan <131908005+data-dev-arav@users.noreply.github.com>
2023-08-02 00:18:11 +0530

    Create Python_While_Loop.py
    Python While Loop

---
+++ Python_While_Loop.py
@@ -0,0 +1,17 @@
+# Syntax for while loop
+# while exp:
+#   statements
+
+ # Write a program to print the table of 9
+num = 9
+counter = 1
+
+while counter<=10: # Either true or false
+    ans = num * counter
+    print(num, '*', counter, '=',ans)
+    counter = counter + 1
+
+
+# Infinite Loop
+while True:
+    print("Arvind")
